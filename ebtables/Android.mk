b6222047dc1717edf620d4ea45822318d617ccb2
dhacker29 <dhackerdvm@gmail.com>
2014-05-11 07:37:38 -0400

    Remove local copy of ebtables as it is tracked now
    Change-Id: Iddb46f88c4ede3d5dfeb96c0f11fc17825bca078

--- ebtables/Android.mk
+++
@@ -1,6 +0,0 @@
-#Avoid building ebtables for emulator
-ifeq ($(call is-vendor-board-platform,QCOM),true)
-    include $(call all-subdir-makefiles)
-else
-    $(info "ebtables is disabled on this build")
-endif
