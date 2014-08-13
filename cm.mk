ebceda34fc6b8502565e6d627807ea0e998c0957
Ethan Chen <intervigil@gmail.com>
2014-08-13 14:09:06 -0700

    msm8226-common: Remove device-specific bits, make configs common
    Change-Id: Ide98418631a6b89b1461e9e7748cdb5ed2857812

--- cm.mk
+++
@@ -1,7 +0,0 @@
-$(call inherit-product, device/motorola/falcon/full_falcon.mk)
-
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
-PRODUCT_RELEASE_NAME := MOTO G
-PRODUCT_NAME := cm_falcon
