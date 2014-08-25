0565c23abde291cdf25cffe7c90b85bba1ccfbce
Paras Nagda <pnagda@codeaurora.org>
2014-08-25 11:38:49 +0530

    msm8226-common: Add shell property to use AwesomePlayer as default.
    This overrides the default player (NuPlayer) from being picked.
    
    Change-Id: I24234a52a6ee3311b95560784ddc271a67774861

--- system_prop.mk
+++ system_prop.mk
@@ -59,6 +59,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.gps.qc_nlp_in_use=1 \
     persist.loc.nlp_name=com.qualcomm.services.location
 
+# Media
+PRODUCT_PROPERTY_OVERRIDEs += \
+    media.stagefright.use-awesome=true
+
 # NITZ
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.rild.nitz_plmn="" \
