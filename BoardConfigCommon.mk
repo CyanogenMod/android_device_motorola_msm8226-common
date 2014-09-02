87671a03df56413b2656757b9e048382ce09b06b
Konsta <konsta09@gmail.com>
2014-09-02 12:32:00 -0700

    Revert "msm8226-common: Use correct CPU flags"
    This partially reverts commit c56603a09ef5d588eaf99d93092baf38c6daac93.
    
    Change-Id: I62a33f5e4fa0870392751d22a1fd50449b000b7c

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -32,7 +32,7 @@ TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_SMP := true
-TARGET_CPU_VARIANT := cortex-a7
+TARGET_CPU_VARIANT := krait
 
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := MSM8226
