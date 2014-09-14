fd78a6fc7607aa7cdedd08109220c1fd7961f2a1
Rabih Salamey <somcom3@gmail.com>
2014-09-14 12:02:16 -0400

    msm8226: Add titan to build barrier
       * Titan is very close to the falcon.
       * Titan is msm8226
    
    Change-Id: I2f734b0dc67dd27008b5f5e2e8ce5972a43cfacb

--- Android.mk
+++ Android.mk
@@ -21,7 +21,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter falcon peregrine,$(TARGET_DEVICE)),)
+ifneq ($(filter falcon peregrine titan,$(TARGET_DEVICE)),)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)
