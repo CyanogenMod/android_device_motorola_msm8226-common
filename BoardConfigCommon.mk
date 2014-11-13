ff0b0e051771a983c8c3d93ca6186aced2c8bf77
Rabih Salamey <somcom3@gmail.com>
2014-11-13 06:06:33 +0000

    Revert "msm8226-common: Fix boot jars classpath"
    This reverts commit 4e67188253e7bae7aee654ef18b128af4b898fa4.
    
    Change-Id: I93f5d8b0096bb1afcc29f428c13686869bf79a6c

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -67,9 +67,6 @@ BOARD_HAVE_BLUETOOTH_QCOM := true
 # Camera
 USE_DEVICE_SPECIFIC_CAMERA := true
 
-# Classpath
-PRODUCT_BOOT_JARS := $(subst $(space),:,$(PRODUCT_BOOT_JARS))
-
 # CMHW
 BOARD_HARDWARE_CLASS := $(LOCAL_PATH)/cmhw/
 
