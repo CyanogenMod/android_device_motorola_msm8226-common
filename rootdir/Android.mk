7af3ac76cca76828d40396d56e3a44ea5943c660
Ethan Chen <intervigil@gmail.com>
2014-12-26 16:00:57 -0800

    msm8226-common: Remove gpecheck script
    * Unnecessary since fstab can contain multiple file system type entries
      now.
    
    Change-Id: I3bff26489661613446e434225fc8ef11521a144a

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -27,22 +27,6 @@ include $(BUILD_PREBUILT)
 # Init scripts
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := bbx
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/bbx
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)/sbin
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := gpecheck.sh
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/gpecheck.sh
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := init.mmi.boot.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
