72d35461fbb38c4943eeb807c214cdb056b842e8
Simon Sickle <simon@simonsickle.com>
2015-01-04 19:46:59 -0600

    msm8266-common: Vendor files should be included if-exists
    It is standard behavior in CM to only include vendor files 'if-exist'
    and therefore, for those possibly only wanting to build a kernel
    this change should be used. By not using this, you have to download
    vendor files to build even a boot.img. For someone who does not own
    the device, this is considered a breach in the blobs agreement and
    this person could possibly be held liable for a license violation.
    
    Change-Id: Ic5e0c40b357e895a53a284a5fadd58d131c67113
    Signed-off-by: Simon Sickle <simon@simonsickle.com>

--- msm8226.mk
+++ msm8226.mk
@@ -16,7 +16,7 @@
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-$(call inherit-product, vendor/motorola/msm8226-common/msm8226-common-vendor.mk)
+$(call inherit-product-if-exists, vendor/motorola/msm8226-common/msm8226-common-vendor.mk)
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
