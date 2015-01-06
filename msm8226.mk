e9d61f60f6e37f51563916806e903d73495eb4c1
Konsta <konsta09@gmail.com>
2015-01-06 18:20:41 +0200

    msm8226-common: Update audio flags and properties
    * Remove no op flags
    * Remove deprecated audio policy HAL package
    * Add required flag to build tunnel mode encoder packages
    * Update PCM offload properties
    
    Change-Id: I065f0e6dc8924962db5861b1194e44e86bec1610

--- msm8226.mk
+++ msm8226.mk
@@ -65,8 +65,7 @@ PRODUCT_PACKAGES += \
     audio.a2dp.default \
     audio.primary.msm8226 \
     audio.r_submix.default \
-    audio.usb.default \
-    audio_policy.msm8226
+    audio.usb.default
 
 PRODUCT_PACKAGES += \
     libaudio-resampler \
