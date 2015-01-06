a30c942c1c7e76921d3f0aed37f0f575e706502e
Konsta <konsta09@gmail.com>
2015-01-06 18:40:01 +0200

    msm8226-common: bluetooth: Update for lollipop
    Bring in-line with LA.BF.1.1.2_rb1.10.
    Combination of:
    
    Author: Pramod Sivaraman <spramod@codeaurora.org>
    Date:   Fri Apr 25 13:52:17 2014 +0530
    
        bluetooth: Defining the MAX L2CAP channels
    
        Defining the MAX_L2CAP_CHANNEL supported as 16
    
        Change-Id: Id455566e2b43214c93a07305ca7e67a5c33e1a77
        CRs-Fixed: 652761
    
    Author: Srinu Jella <sjella@codeaurora.org>
    Date:   Thu Feb 20 11:34:07 2014 +0530
    
        bluetooth: Disable Interleave scan feature from Host
    
        Disable Interleave Bluetooth scan feature from host side
        as it is enabled from SOC.
    
        It's revert of I3c4672d3758ef2ae7a093cd6f121db28ea1672ce.
    
        CRs-Fixed: 628746
        Change-Id: I22748b6707a62f9dddf23ff81250770875c9ef0b
    
    Author: Subramanian Srinivasan <subrsrin@codeaurora.org>
    Date:   Wed Feb 19 11:59:39 2014 -0800
    
        bluetooth: Changes Bluedroid configuration for BLE Peripheral mode
    
        Enables read remote device feature and connection
        update features in bluedroid configuration for
        BLE peripheral mode implementation.
    
        Change-Id: Ifc38df532e6d7b7800097f879d943e950c4f5fd1
        CRs-fixed: 626326
    
    Author: AnubhavGupta <anubhavg@codeaurora.org>
    Date:   Wed Aug 20 21:38:12 2014 +0530
    
        overlay: Bluetooth: Enable A2DP Sink
    
         - Enable A2DP Sink
    
        Change-Id: Ifd438e3acbe43887c290b11b3d9083f105f93a2f
    
    Author: Mallikarjuna GB <gbmalli@codeaurora.org>
    Date:   Wed Jul 30 23:29:43 2014 +0530
    
        overlay: Bluetooth: Enable HFP HS and AVRCP controller
    
        Enable HFP HS and AVRCP controller
    
        Change-Id: I7c47aef3cdfeec4613ece0d7610546cece5512b0
    
    Change-Id: I9d5dbcb26dc4252d18f504bd518dd312968b644b

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -19,12 +19,10 @@
 #define _BDROID_BUILDCFG_H
 
 #define BTM_DEF_LOCAL_NAME   "Moto G"
-#define BLUETOOTH_QCOM_SW TRUE
-// Enables Interleave scan
-#define BTA_HOST_INTERLEAVE_SEARCH  TRUE
+#define BLUETOOTH_QTI_SW TRUE
 // Disables read remote device feature
-#define BTA_SKIP_BLE_READ_REMOTE_FEAT TRUE
-#define MAX_L2CAP_CHANNELS    14
+#define BTA_SKIP_BLE_READ_REMOTE_FEAT FALSE
+#define MAX_L2CAP_CHANNELS    16
 // skips conn update at conn completion
-#define BTA_BLE_SKIP_CONN_UPD  TRUE
+#define BTA_BLE_SKIP_CONN_UPD  FALSE
 #endif
