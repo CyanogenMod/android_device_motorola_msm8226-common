810d07c920e22f9884102ec1ee3a5d20eaab83a9
Konsta <konsta09@gmail.com>
2015-01-09 20:17:16 +0200

    msm8226-common: proprietary-files: Don't link libtime_genoff
    * No need after f6a49819c57821eae6d7c4781af215a5e62346ff
    
    Change-Id: Ic371b4e32fac85892aa33db7e11d55635e8ee8af

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -157,8 +157,7 @@ PRODUCT_PACKAGES += \\
     imslibrary \\
     qcnvitems \\
     qcrilhook \\
-    libHevcSwDecoder \\
-    libtime_genoff
+    libHevcSwDecoder
 
 \$(call inherit-product, vendor/$VENDOR/$DEVICE/$DEVICE-vendor-blobs.mk)
 EOF
@@ -286,16 +285,6 @@ include \$(CLEAR_VARS)
 LOCAL_MODULE := libHevcSwDecoder
 LOCAL_MODULE_OWNER := $VENDOR
 LOCAL_SRC_FILES := proprietary/vendor/lib/libHevcSwDecoder.so
-LOCAL_MODULE_PATH := \$(TARGET_OUT_VENDOR_SHARED_LIBRARIES)
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_SUFFIX := .so
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-include \$(BUILD_PREBUILT)
-
-include \$(CLEAR_VARS)
-LOCAL_MODULE := libtime_genoff
-LOCAL_MODULE_OWNER := $VENDOR
-LOCAL_SRC_FILES := proprietary/vendor/lib/libtime_genoff.so
 LOCAL_MODULE_PATH := \$(TARGET_OUT_VENDOR_SHARED_LIBRARIES)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_SUFFIX := .so
