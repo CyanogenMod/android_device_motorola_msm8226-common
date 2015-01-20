acfe31b999f22140144e91af4ab2fced2d348019
LuK1337 <priv.luk@gmail.com>
2015-01-20 17:24:12 -0500

    Don't use falcon/peregrine audio config for titan
    Change-Id: I7db35f1bc2d8b4c49b404d8951a6f336ac83de3a

--- msm8226.mk
+++ msm8226.mk
@@ -78,8 +78,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio_effects.conf:system/vendor/etc/audio_effects.conf \
     $(LOCAL_PATH)/configs/audio_platform_info.xml:system/etc/audio_platform_info.xml \
-    $(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf \
-    $(LOCAL_PATH)/configs/mixer_paths.xml:system/etc/mixer_paths.xml
+    $(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf
+
+ifneq ($(filter falcon peregrine,$(TARGET_DEVICE)),)
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/configs/mixer_paths.xml:system/etc/mixer_paths.xml
+endif
 
 # Camera
 PRODUCT_PACKAGES += \
