0b1d58af2bd3ecf7feda0c6f6eaa14d68da441eb
luca020400 <luca.stefani.ge1@gmail.com>
2015-05-02 22:19:50 +0200

    msm8226-common: fix VOIP
    Change-Id: I793dfd2a3ca6ec03f0b143c653930a3b87444f48

--- system_prop.mk
+++ system_prop.mk
@@ -31,7 +31,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.qc.sdk.audio.ssr=false
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    use.voice.path.for.pcm.voip=true
+    use.voice.path.for.pcm.voip=false
 
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
