9d5e9c5b6c2713a3cb071943bf778a388ddc827f
Nick Kralevich <nnk@google.com>
2015-06-02 16:00:21 -0700

    msm8226-common: remove persist.sys.usb.config override
    The variable persist.sys.usb.config is no longer honored by the framework,
    and it's presence causes unnecessary toggling of the USB driver, which
    disonnects ADB and makes the device's connection unstable.
    
    Delete it.
    
    Bug: 21404762
    Bug: 18905620
    Change-Id: Ie067c98a59f6011009f612314c52c49ce9c25e1b

--- msm8226.mk
+++ msm8226.mk
@@ -43,9 +43,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
 
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    persist.sys.usb.config=mtp
-
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
