1f2e2357d6636cc09f17404625223a442f296e4c
luca020400 <luca.stefani.ge1@gmail.com>
2015-06-19 10:23:44 +0200

    msm8226-common: remove bluetooth init script
    * unused after 6cdd5dc
    
    Change-Id: I5d89b296c30f52e7da1559321d929996ce329087

--- msm8226.mk
+++ msm8226.mk
@@ -164,7 +164,6 @@ PRODUCT_PACKAGES += \
 # Ramdisk
 PRODUCT_PACKAGES += \
     init.crda.sh \
-    init.qcom.bt.sh \
     init.qcom.fm.sh \
     mount_pds.sh
 
--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -10,13 +10,6 @@ LOCAL_SRC_FILES    := etc/init.crda.sh
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.qcom.bt.sh
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.qcom.bt.sh
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := mount_pds.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
