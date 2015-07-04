f38e4ad6698846968bb4569caa8610d9bad09d6a
LuK1337 <priv.luk@gmail.com>
2015-07-04 18:08:39 +0200

    msm8226-common: Correctly start ril-daemon1
    * Using the dsds/dsda property fails to properly support the encryption
      usecase. Set up a wrapper script that is subject to the same
      start/restart timing as the actual RIL daemon.
    
    Change-Id: Iaf270587399aa4601b06ef72139014cb3a33fd43

---
+++ rootdir/etc/init.qcom.ril.sh
@@ -0,0 +1,9 @@
+#!/system/bin/sh
+export PATH=/system/xbin:$PATH
+
+multisim=`getprop persist.radio.multisim.config`
+
+if [ "$multisim" = "dsds" ] || [ "$multisim" = "dsda" ]; then
+    stop ril-daemon1
+    start ril-daemon1
+fi
