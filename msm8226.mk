3804c36669e86142bdb0c96299d97141aa87f40a
myfluxi <linflux@arcor.de>
2015-07-21 11:49:39 +0200

    msm8226-common: switch to powerhal profiles
    Change-Id: I83fcad7b66b17e66c7be341ae05744d4aad7d719

--- msm8226.mk
+++ msm8226.mk
@@ -172,6 +172,7 @@ PRODUCT_PACKAGES += \
     init.mmi.boot.sh \
     init.mmi.touch.sh \
     init.qcom.rc \
+    init.qcom.power.rc \
     init.qcom.ssr.sh \
     init.qcom.usb.rc \
     init.recovery.qcom.rc \
