e62be26f6c28717d5ccdb9dcb1d350605c558f43
Konsta <konsta09@gmail.com>
2015-07-29 14:34:46 +0300

    msm8226-common: Misc makefile cleanup
    * Remove makefile entries for build targets that have
      been already removed
    
    Change-Id: I5ed4b8b30bfc0fd4699dec5b57454e1223dcc8b8

--- msm8226.mk
+++ msm8226.mk
@@ -155,9 +155,8 @@ PRODUCT_PACKAGES += \
 # Ramdisk
 PRODUCT_PACKAGES += \
     init.qcom.bt.sh \
-    init.qcom.fm.sh \
     init.qcom.ril.sh \
-    mount_pds.sh \
+    mount_pds.sh
 
 PRODUCT_PACKAGES += \
     init.mmi.boot.sh \
