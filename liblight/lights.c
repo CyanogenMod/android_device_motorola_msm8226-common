400e176c9e6609ef4907c8a2a1442658a70fa5cc
LuK1337 <priv.luk@gmail.com>
2015-08-01 16:40:39 +0200

    msm8226-common: Update liblights
    Change-Id: I73078292588d9b02e250491e0d715f8fb0f9a974

--- liblight/lights.c
+++ liblight/lights.c
@@ -1,5 +1,7 @@
 /*
- * Copyright (C) 2014 The CyanogenMod Project
+ * Copyright (C) 2015 The CyanogenMod Project
+ * Copyright (C) 2008 The Android Open Source Project.
+ * Copyright (C) 2012-2014, The Linux Foundation. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,15 +16,17 @@
  * limitations under the License.
  */
 
+
+// #define LOG_NDEBUG 0
+
 #include <cutils/log.h>
-#include <cutils/properties.h>
+
 #include <stdint.h>
 #include <string.h>
 #include <unistd.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <pthread.h>
-#include <math.h>
 
 #include <sys/ioctl.h>
 #include <sys/types.h>
@@ -30,8 +34,6 @@
 #include <hardware/lights.h>
 
 /******************************************************************************/
-
-#define MAX_PATH_SIZE 80
 
 static pthread_once_t g_init = PTHREAD_ONCE_INIT;
 static pthread_mutex_t g_lock = PTHREAD_MUTEX_INITIALIZER;
@@ -67,8 +69,8 @@ write_int(char const* path, int value)
     fd = open(path, O_RDWR);
     if (fd >= 0) {
         char buffer[20];
-        int bytes = sprintf(buffer, "%d\n", value);
-        int amt = write(fd, buffer, bytes);
+        int bytes = snprintf(buffer, sizeof(buffer), "%d\n", value);
+        ssize_t amt = write(fd, buffer, (size_t)bytes);
         close(fd);
         return amt == -1 ? -errno : 0;
     } else {
@@ -134,7 +136,7 @@ set_speaker_light_locked(struct light_device_t* dev,
 {
     int len;
     int blink;
-    int onMS, offMS, ramp;
+    int onMS, offMS;
     unsigned int colorRGB;
     char blink_pattern[PAGE_SIZE];
 
@@ -152,24 +154,25 @@ set_speaker_light_locked(struct light_device_t* dev,
 
     colorRGB = state->color;
 
-    if (onMS > 0 && offMS > 0) {
+#if 0
+    ALOGD("set_speaker_light_locked mode %d, colorRGB=%08X, onMS=%d, offMS=%d\n",
+            state->flashMode, colorRGB, onMS, offMS);
+#endif
 
+    if (onMS > 0 && offMS > 0) {
         blink = 1;
-        ramp = 1;
     } else {
         blink = 0;
-        ramp = 0;
     }
 
-    // See hardware/libhardware/include/hardware/lights.h
-    int brightness = ((77 * ((colorRGB >> 16) & 0xFF)) +
-                      (150 * ((colorRGB >> 8) & 0xFF)) +
-                      (29 * (colorRGB & 0xFF))) >> 8;
-    write_int(WHITE_LED_FILE, (int) brightness);
-
     if (blink) {
-        sprintf(blink_pattern,"%6x %d %d %d %d",colorRGB,onMS,offMS,ramp,ramp);
+        sprintf(blink_pattern, "%6x %d %d %d %d", colorRGB, onMS, offMS, blink, blink);
         write_str(RGB_CONTROL_FILE, blink_pattern);
+    } else {
+        int brightness = ((77 * ((colorRGB >> 16) & 0xFF)) +
+                          (150 * ((colorRGB >> 8) & 0xFF)) +
+                          (29 * (colorRGB & 0xFF))) >> 8;
+        write_int(WHITE_LED_FILE, (int) brightness);
     }
 
     return 0;
@@ -211,7 +214,6 @@ set_light_attention(struct light_device_t* dev,
     return 0;
 }
 
-
 /** Close the lights device */
 static int
 close_lights(struct light_device_t *dev)
@@ -248,6 +250,10 @@ static int open_lights(const struct hw_module_t* module, char const* name,
     pthread_once(&g_init, init_globals);
 
     struct light_device_t *dev = malloc(sizeof(struct light_device_t));
+
+    if(!dev)
+        return -ENOMEM;
+
     memset(dev, 0, sizeof(*dev));
 
     dev->common.tag = HARDWARE_DEVICE_TAG;
