b76aa514604c547542dd8569d011e96a98804222
Alberto97 <albertop2197@gmail.com>
2015-08-25 20:54:29 +0200

    msm8226-common: Move system.prop to the common device tree
      * It's useless keeping the same value of rild.libargs
        in 4 different device trees
      * Move system_prop.mk to system.prop
    
    Change-Id: I2e774c595c1d3dec04328124a03c1067746b1d7c

--- msm8226.mk
+++ msm8226.mk
@@ -43,9 +43,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
 
-# System Properties
--include $(LOCAL_PATH)/system_prop.mk
-
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     persist.sys.usb.config=mtp
 
