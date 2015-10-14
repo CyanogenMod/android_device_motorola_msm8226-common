0f0d5601d41afa40af395c66a918a9f1b7fdf68f
Matt Mower <mowerm@gmail.com>
2015-10-14 13:25:19 -0700

    msm8226-common: Provide global moto symbols via libmoto
    Change-Id: I0fccaa429ba5e87ec3601adcc20c913ba364c14a

--- msm8226.mk
+++ msm8226.mk
@@ -129,6 +129,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
     $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
 
+# Motorola
+PRODUCT_PACKAGES += \
+    libmoto
+
 # OMX
 PRODUCT_PACKAGES += \
     libdashplayer \
