2df543761a5924de60acbb0b79c247e937eaf53b
Gabriele M <moto.falcon.git@gmail.com>
2015-10-25 20:55:16 +0100

    msm8226-common: Add falcon and peregrine sensors HAL
    This sensors HAL supports both falcon and peregrine. The support for
    the gyroscope is included depending on the target device.
    
    It supports all the sensors supported by the stock sensors HAL with
    the addition of a Significant Motion sensor. Since the Display Rotation
    sensor and the Significant Motion sensor are mutually exclusive, the
    HAL doesn't expose the Display Rotation sensor to avoid conflicts,
    unless DISPLAY_ROTATION_SENSOR_ENABLED is set to true.
    
    Kernel changes are required for this sensors HAL to work properly.
    
    Change-Id: I1130bff103f3efdbb7315c068ea4b81d8f6b8955

---
+++ libsensors/falcon-peregrine/Android.mk
@@ -0,0 +1,53 @@
+# Copyright (C) 2015 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+LOCAL_PATH := $(call my-dir)
+
+# HAL module implemenation, not prelinked, and stored in
+# hw/<SENSORS_HARDWARE_MODULE_ID>.<ro.product.board>.so
+include $(CLEAR_VARS)
+
+# Display Rotation sensor and Significant Motion sensor are mutually
+# exclusive. If the following is set to true, the HAL will expose both
+# the sensors, possibly leading to conflicts.
+DISPLAY_ROTATION_SENSOR_ENABLED := false
+
+LOCAL_MODULE := sensors.$(TARGET_BOARD_PLATFORM)
+
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_CFLAGS := -DLOG_TAG=\"msm8226_Sensors\"
+
+LOCAL_SRC_FILES := \
+    sensors.cpp \
+    SensorBase.cpp \
+    LightProxSensor.cpp \
+    AccelerometerSensor.cpp \
+    CompOriSensor.cpp \
+    InputEventReader.cpp
+
+ifneq ($(filter peregrine, $(TARGET_DEVICE)),)
+    LOCAL_CFLAGS += -DWITH_GYROSCOPE
+    LOCAL_SRC_FILES += GyroscopeSensor.cpp
+endif
+
+ifeq ($(DISPLAY_ROTATION_SENSOR_ENABLED), true)
+    LOCAL_CFLAGS += -DWITH_DISPLAY_ROTATION_SENSOR
+endif
+
+LOCAL_SHARED_LIBRARIES := liblog libcutils libdl
+
+include $(BUILD_SHARED_LIBRARY)
