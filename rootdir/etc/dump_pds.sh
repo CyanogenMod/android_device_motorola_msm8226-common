ca7c405720df213b73cd49b4ff0a07f914ce8b1a
Gabriele M <moto.falcon.git@gmail.com>
2015-11-07 18:12:35 +0100

    msm8226-common: Allow the use of PDS dumps with SELinux
    Use the inbuilt init command to mount the dump using a loop device.
    This allows to get around the SELinux neverallow rule that grants
    the mount permission to few selected domains.
    
    Change also the name of the dump and the name of the script to better
    reflect its function.
    
    Change-Id: If62e5f8679ee6da1523def7f309c081aa1855f6a

---
+++ rootdir/etc/dump_pds.sh
@@ -0,0 +1,10 @@
+#!/system/bin/sh
+
+DUMP=/cache/pds-dump.img
+
+if [ ! -f $DUMP ]; then
+    # Make a copy of pds in /cache
+    /system/xbin/dd if=/dev/block/platform/msm_sdcc.1/by-name/pds of=$DUMP-temp
+    # Prevent init from using a partially created file
+    mv $DUMP-temp $DUMP
+fi
