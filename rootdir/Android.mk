8607c296a80d5c35e357d98ecbc4f09a60529f0d
Gabriele M <moto.falcon.git@gmail.com>
2015-11-07 18:12:35 +0100

    msm8226-common: Allow the use of PDS dumps with SELinux
    Use the inbuilt init command to mount the dump using a loop device.
    This allows to get around the SELinux neverallow rule that grants
    the mount permission to few selected domains.
    
    Change also the name of the dump and the name of the script to better
    reflect its function.
    
    Change-Id: If62e5f8679ee6da1523def7f309c081aa1855f6a

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -17,10 +17,10 @@ LOCAL_SRC_FILES    := etc/init.qcom.ril.sh
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := mount_pds.sh
+LOCAL_MODULE       := dump_pds.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/mount_pds.sh
+LOCAL_SRC_FILES    := etc/dump_pds.sh
 LOCAL_MODULE_PATH  := $(TARGET_OUT_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
