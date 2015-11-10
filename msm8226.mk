3c32ea165748f3a070acd5b86f39d4983ae7d670
Adam Farden <adam@farden.cz>
2015-11-10 14:42:43 +0100

    msm8226-common: Use OSS TimeKeep service by Sony
     * Remembers the time set over reboots correctly
     * Fix the permanent errors :
       "libc : page record for 0x... was not found (block_size=16)"
    
    Change-Id: I50e72663169ee2907e248f634a9d1a4d3792c227
    Signed-off-by: Adam Farden <adam@farden.cz>

--- msm8226.mk
+++ msm8226.mk
@@ -46,6 +46,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     persist.sys.usb.config=mtp
 
+# Sony timekeep
+PRODUCT_PACKAGES += \
+    timekeep \
+    TimeKeep
+
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
