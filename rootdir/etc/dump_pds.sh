ffc80204b17d2cb83e103e939ddedfe07a591740
Luca Stefani <luca.stefani.ge1@gmail.com>
2015-11-21 07:11:03 -0800

    msm8226-common: update dump_pds.sh
    * DD has been moved into /system/bin/
    
    Change-Id: I21f61f78ebe404d11c2d1d4f188ea2b876490972

--- rootdir/etc/dump_pds.sh
+++ rootdir/etc/dump_pds.sh
@@ -4,7 +4,7 @@ DUMP=/cache/pds-dump.img
 
 if [ ! -f $DUMP ]; then
     # Make a copy of pds in /cache
-    /system/xbin/dd if=/dev/block/platform/msm_sdcc.1/by-name/pds of=$DUMP-temp
+    /system/bin/dd if=/dev/block/platform/msm_sdcc.1/by-name/pds of=$DUMP-temp
     # Prevent init from using a partially created file
     mv $DUMP-temp $DUMP
 fi
