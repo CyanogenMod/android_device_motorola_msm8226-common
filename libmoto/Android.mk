a9366b76c88393e60eff42dfc0aa6a445179e896
LuK1337 <priv.luk@gmail.com>
2015-11-22 14:32:30 +0100

    msm8226-common: Update blobs to M
    * Motorola package version: titan_retbr_dstv-user 6.0 MPB24.65-10 10
    
    Change-Id: I4583b21a3bb92f7e46385754a0fb9f512627b9c6

--- libmoto/Android.mk
+++ libmoto/Android.mk
@@ -14,33 +14,12 @@
 
 LOCAL_PATH := $(call my-dir)
 
-# Camera
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := moto_camera.c
-
-LOCAL_SHARED_LIBRARIES := libgui liblog libutils
-LOCAL_MODULE := libmoto_camera
-LOCAL_MODULE_TAGS := optional
-
-include $(BUILD_SHARED_LIBRARY)
-
 # Log
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := moto_log.c
 
 LOCAL_MODULE := libmoto_log
-LOCAL_MODULE_TAGS := optional
-
-include $(BUILD_SHARED_LIBRARY)
-
-# RIL
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := moto_ril.c
-
-LOCAL_MODULE := libmoto_ril
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
