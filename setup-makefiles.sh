635fdaae90eb7b9c9d9ad6b5c77ff13a33e0de5c
Steve Kondik <steve@cyngn.com>
2015-11-29 05:37:11 -0800

    msm8226-common: Cleanups
    * Drop all the janky proprietary stuff we don't even use
    * Replace qcom_diag with diag
    
    Change-Id: Ib97b12ef79965f16088821c8299e09a78baaeb3d

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -154,8 +154,7 @@ PRODUCT_PACKAGES += \\
     TimeService \\
     qcrilmsgtunnel \\
     qcnvitems \\
-    qcrilhook \\
-    libHevcSwDecoder
+    qcrilhook
 
 \$(call inherit-product, vendor/$VENDOR/$DEVICE/$DEVICE-vendor-blobs.mk)
 EOF
@@ -257,16 +256,6 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_SUFFIX := \$(COMMON_JAVA_PACKAGE_SUFFIX)
 LOCAL_MODULE_CLASS := JAVA_LIBRARIES
 LOCAL_CERTIFICATE := PRESIGNED
-include \$(BUILD_PREBUILT)
-
-include \$(CLEAR_VARS)
-LOCAL_MODULE := libHevcSwDecoder
-LOCAL_MODULE_OWNER := $VENDOR
-LOCAL_SRC_FILES := proprietary/vendor/lib/libHevcSwDecoder.so
-LOCAL_MODULE_PATH := \$(TARGET_OUT_VENDOR_SHARED_LIBRARIES)
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_SUFFIX := .so
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 include \$(BUILD_PREBUILT)
 
 endif
