d9a943b7954778a046a1e0f4d1f349e26f39c8bf
Gabriele M <moto.falcon.git@gmail.com>
2016-01-06 01:11:06 +0100

    msm8226-common: Swith to zImage-dtb
    Testing changes to the device tree using fastboot boot is not possible
    if it's kept separate from the zImage. The new kernel boots using the
    old device tree blob. This doesn't happen when the dtb is appended to
    the zImage, so switch to zImage-dtb.
    
    Change-Id: Iac3038a9a2ff6344cbdcd9e4a89a4e95dbda6dfa

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -44,8 +44,7 @@ TARGET_NO_BOOTLOADER := true
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.bootdevice=msm_sdcc.1 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 vmalloc=400M utags.blkdev=/dev/block/platform/msm_sdcc.1/by-name/utags
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_KERNEL_SEPARATED_DT := true
-BOARD_DTBTOOL_ARGS := --force-v2
+BOARD_KERNEL_IMAGE_NAME := zImage-dtb
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 TARGET_KERNEL_SOURCE := kernel/motorola/msm8226
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androideabi-
