d226427577a7f373536d13cb4a58f77eec3ba55b
Gabriele M <moto.falcon.git@gmail.com>
2016-01-11 23:40:57 +0100

    msm8226-common: Add a bunch of extra camera parameters
    Also, rename the existing ones to differentiate the QCOM specific
    parameters from the Motorola ones.
    
    Change-Id: Ib2ff8967afc4a64fe6cbf497536f08374a11c351

--- camera/CameraWrapper.cpp
+++ camera/CameraWrapper.cpp
@@ -145,7 +145,7 @@ static char *camera_fixup_getparams(int id, const char *settings)
 #endif
 
     if (id == BACK_CAMERA) {
-        params.set(CameraParameters::KEY_SUPPORTED_TOUCH_AF_AEC, "touch-on,touch-off");
+        params.set(CameraParameters::KEY_QC_SUPPORTED_TOUCH_AF_AEC, "touch-on,touch-off");
     }
 
 #if !LOG_NDEBUG
@@ -190,7 +190,7 @@ static char *camera_fixup_setparams(int id, const char *settings)
         const char *sceneMode = params.get(CameraParameters::KEY_SCENE_MODE);
         if (sceneMode != NULL) {
             if (!strcmp(sceneMode, CameraParameters::SCENE_MODE_HDR)) {
-                params.remove(CameraParameters::KEY_ZSL);
+                params.remove(CameraParameters::KEY_QC_ZSL);
             }
         }
     }
