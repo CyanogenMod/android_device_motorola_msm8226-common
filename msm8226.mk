b93fc85040a8ad42be6ef2b01d8ebe617110c1f1
Gabriele M <moto.falcon.git@gmail.com>
2016-05-10 11:59:12 +0200

    msm8226-common: Remove common audio_platform_info.xml
    Each variant needs its own platform configuration file, so remove it
    from here.
    
    Change-Id: I4266d28264a487ce28d0c0f5461070f53be0d912

--- msm8226.mk
+++ msm8226.mk
@@ -79,7 +79,6 @@ PRODUCT_PACKAGES += \
 # Audio configuration
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio_effects.conf:system/vendor/etc/audio_effects.conf \
-    $(LOCAL_PATH)/configs/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf
 
 # Camera
