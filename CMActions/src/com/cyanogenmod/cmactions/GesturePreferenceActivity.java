3a0ae9cc1d52c07480e7e17f408a144f4f4e0082
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-09-03 04:41:00 -0500

    msm8226-common: cmactions: Remove back button from action bar
    Starting N, top-level Settings activities should a drawer menu up
    on the action bar. Remove the up/back button as it is now obsolete.
    
    Change-Id: Iad035bc484f37329e67ed0247ff947849ec689c7

--- CMActions/src/com/cyanogenmod/cmactions/GesturePreferenceActivity.java
+++ CMActions/src/com/cyanogenmod/cmactions/GesturePreferenceActivity.java
@@ -18,25 +18,14 @@ package com.cyanogenmod.cmactions;
 
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
-import android.view.MenuItem;
 
 public class GesturePreferenceActivity extends PreferenceActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getActionBar().setDisplayHomeAsUpEnabled(true);
         getFragmentManager().beginTransaction()
                 .replace(android.R.id.content, new GesturePreferenceFragment()).commit();
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                onBackPressed();
-                return true;
-        }
-        return false;
-    }
 }
